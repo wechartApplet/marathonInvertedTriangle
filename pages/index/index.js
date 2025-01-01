9b57d67ac643f3b6df148136808fb7bab3110bcf
涎涎 <yanbo0039@yeah.net>
2025-01-01 18:38:11 +0800

    传输数据以及新新paceChart模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,120 +2,93 @@
 // 用户没有上传头像时的占位图
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0';
 // 导入时间的数值
-const timePicker = require('../../utils/timePicker.js'); 
+const timePicker = require('../../utils/timePicker.js');
 // 导入距离的数值
-const distancePicker = require('../../utils/distancePicker.js'); 
+const distancePicker = require('../../utils/distancePicker.js');
 // 导入公共的函数
 const common = require('../../utils/common.js');
 
 // pages/index/index.js
 Page({
-  data: {
-    timeOptions: [], // 示例时间选项
-    selectedIndex: 0, // 默认选择的索引
-
-    initialTimeIndex: 0, // 默认初始时间索引
-    sprintTimeIndex: 0, // 默认冲刺时间索引
-    distanceIndex: 0,// 默认距离索引
-    paceData: []
-  },
-  onLoad: function() {
-    // 加载时将生成的时间值和距离值进行对应的赋值
-    this.setData({
-      timeOptions: timePicker.generateTimeList(),
-      distanceOptions: distancePicker.generateDistanceOptions()
-    });
-    this.setDefaultIndices();
-  },
-
-  // 设置默认的初始配速 和 冲刺配速 和 运动距离
-  setDefaultIndices: function() {
-    const initialIndex = common.getIndex('06:30',this.data.timeOptions);
-    const sprintIndex = common.getIndex( '04:30',this.data.timeOptions);
-    //  这里必须得是数字
-    const distanceIndex = common.getIndex(21.0975,this.data.distanceOptions);
-    this.setData({
-      initialTimeIndex: initialIndex !== -1 ? initialIndex : 0,
-      sprintTimeIndex: sprintIndex !== -1 ? sprintIndex : 0,
-      distanceIndex: distanceIndex !== -1 ? distanceIndex : 0
-    });
-  },
-
-  // 当用户选择及时更新页面
-  bindPickerChange: function(e) {
-    this.setData({
-      selectedIndex: e.detail.value
-    });
-    const type = e.currentTarget.dataset.type; // 'initial' 或 'sprint' 或 'distance'
-    const index = e.detail.value;
-    switch (type) {
-      case 'initial':
+    data: {
+        timeOptions: [], // 示例时间选项
+        selectedIndex: 0, // 默认选择的索引
+        initialTimeIndex: 0, // 默认初始时间索引
+        sprintTimeIndex: 0, // 默认冲刺时间索引
+        distanceIndex: 0,// 默认距离索引
+        paceData: []
+    },
+    onLoad: function () {
+        // 加载时将生成的时间值和距离值进行对应的赋值
         this.setData({
-          initialTimeIndex: index,
-          initialTime: this.data.timeOptions[index] // 更新显示的时间
+            timeOptions: timePicker.generateTimeList(),
+            distanceOptions: distancePicker.generateDistanceOptions()
         });
-        break;
-      case 'sprint':
+        this.setDefaultIndices();
+    },
+
+    // 设置默认的初始配速 和 冲刺配速 和 运动距离
+    setDefaultIndices: function () {
+        const initialIndex = common.getIndex('06:30', this.data.timeOptions);
+        const sprintIndex = common.getIndex('04:30', this.data.timeOptions);
+        //  这里必须得是数字
+        const distanceIndex = common.getIndex(21.0975, this.data.distanceOptions);
         this.setData({
-          sprintTimeIndex: index,
-          sprintTime: this.data.timeOptions[index] // 更新显示的时间
+            initialTimeIndex: initialIndex !== -1 ? initialIndex : 0,
+            sprintTimeIndex: sprintIndex !== -1 ? sprintIndex : 0,
+            distanceIndex: distanceIndex !== -1 ? distanceIndex : 0
         });
-        break;
-      case 'distance':
+    },
+
+    // 当用户选择及时更新页面
+    bindPickerChange: function (e) {
         this.setData({
-          distanceIndex: index,
-          distance: this.data.distanceOptions[index] // 更新显示的距离
+            selectedIndex: e.detail.value
         });
-        break;
-    }
-  },
-
-
-
-
-  calculatePace: function() {
-    // const initialTime = this.data.timeOptions[this.data.initialTimeIndex];
-    // const sprintTime = this.data.timeOptions[this.data.sprintTimeIndex];
-    // const distance = parseInt(this.data.distance, 10);
-    // const initialSeconds = this.convertToSeconds(initialTime);
-    // const sprintSeconds = this.convertToSeconds(sprintTime);
-    // const totalSeconds = initialSeconds - sprintSeconds;
-    // const pacePerKm = totalSeconds / distance;
-    // let paceData = [];
-
-    // for (let i = 1; i <= distance; i++) {
-    //   let currentSeconds = initialSeconds - (pacePerKm * i);
-    //   paceData.push({
-    //     km: i,
-    //     pace: this.formatTime(currentSeconds)
-    //   });
-    // }
-
-    // this.setData({
-    //   paceData: paceData
-    // });
-
-    // console.log('Calculating pace...');
-    // console.log(this.data.paceData);
-    let paceDataString = JSON.stringify(this.data.paceData);
-    // console.log('Pace data stringified:', paceDataString);
-  
-    wx.navigateTo({
-      // url: 'pages/paceChart/paceChart?paceData=' + paceDataString
-      url: '/pages/paceChart/index'
-    });
-
-  },
-
-  // convertToSeconds: function(timeStr) {
-  //   const [hours, minutes, seconds] = timeStr.split(':').map(Number);
-  //   return hours * 3600 + minutes * 60 + seconds;
-  // },
-
-  // formatTime: function(seconds) {
-  //   const hours = Math.floor(seconds / 3600);
-  //   const minutes = Math.floor((seconds % 3600) / 60);
-  //   const secs = seconds % 60;
-  //   return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-  // }
+        const type = e.currentTarget.dataset.type; // 'initial' 或 'sprint' 或 'distance'
+        const index = e.detail.value;
+        switch (type) {
+            case 'initial':
+                this.setData({
+                    initialTimeIndex: index,
+                    initialTime: this.data.timeOptions[index] // 更新显示的时间
+                });
+                break;
+            case 'sprint':
+                this.setData({
+                    sprintTimeIndex: index,
+                    sprintTime: this.data.timeOptions[index] // 更新显示的时间
+                });
+                break;
+            case 'distance':
+                this.setData({
+                    distanceIndex: index,
+                    distance: this.data.distanceOptions[index] // 更新显示的距离
+                });
+                break;
+        }
+    },
+    // 点击提交跳转生成对应的页面
+    calculatePace: function () {
+        //1. 先取到 初始配束
+        const initialTime = common.convertTimeToSeconds(this.data.timeOptions[this.data.initialTimeIndex]);
+        //2. 取冲刺配速
+        const sprintTime = common.convertTimeToSeconds(this.data.timeOptions[this.data.sprintTimeIndex]);
+        //3. 取运动距离
+        const distance = this.data.distanceOptions[this.data.distanceIndex];
+        //4. 将时间转化为秒
+        //5. 将其全部存入数组
+        const paceDataObj = {
+            initialTime: initialTime,
+            sprintTime: sprintTime,
+            distance: distance
+        };
+        this.data.paceData.push(paceDataObj);
+        //6. 将数组序列化
+        let paceDataString = JSON.stringify(this.data.paceData);
+        //7. 跳转页面
+        wx.navigateTo({
+            url: '/pages/paceChart/paceChart?paceData=' + paceDataString
+        });
+    },
 });
--- utils/common.js
+++ utils/common.js
@@ -4,10 +4,26 @@
  * @param {*} item 元素
  * @param {*} array 包含元素的数组
  */
-function getIndex(item,array){
-  return array.indexOf(item);
+function getIndex(item, array) {
+    return array.indexOf(item);
+}
+/**
+ * 
+ * @param {*} timeStr 传入时间 格式为 XX:XX:XX 或 XX:XX 或 XX
+ */
+function convertTimeToSeconds(timeStr) {
+    // 首先，通过冒号":"分割字符串
+    const parts = timeStr.split(':');
+    // 将小时转换为秒（小时数 * 3600）
+    const hoursInSec = parseInt(parts[0], 10) * 3600;
+    // 将分钟转换为秒（分钟数 * 60）
+    const minutesInSec = parseInt(parts[1], 10) * 60;
+    // 将小时和分钟的秒数相加得到总秒数
+    const totalSeconds = hoursInSec + minutesInSec;
+    return totalSeconds;
 }
 
 module.exports = {
-  getIndex
+    getIndex,
+    convertTimeToSeconds
 }
